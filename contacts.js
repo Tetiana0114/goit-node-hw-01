b2753f8d72069f2e649d1dafe2de9f548a1b603b
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2022-12-30 21:44:13 +0200

    step 2-3

---
+++ contacts.js
@@ -0,0 +1,50 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.resolve(__dirname, "./db/contacts.json");
+
+async function readContacts() {
+  const contactsRaw = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(contactsRaw);
+  return contacts;
+}
+
+async function writeContacts(db) {
+  await fs.writeFile(contactsPath, JSON.stringify(db, null, 4));
+}
+
+
+async function listContacts() {
+    const contacts = await readContacts();
+    return contacts;
+}
+
+async function getContactById(contactId) {
+    const contacts = await readContacts();
+    const contactById = contacts.find((contact) => contact.id === contactId);
+    return contactById;
+}
+
+async function removeContact(contactId) {
+    const contacts = readContacts();
+    const updatedContacts = contacts.filter((contact) => contact.id !== contactId);
+    await writeContacts(updatedContacts);
+}
+
+async function addContact(name, email, phone) {
+    const id = nanoid(4);
+    const contact = { id, name, email, phone };
+
+    const db = await readContacts();
+    db.push(contact);
+
+    await writeContacts(db);
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('test');
+const { addContact, listContacts, removeContact, getContactById } = require("./contacts");
+
+
+async function invokeAction({ action, name, email, phone, id }) {
+  switch (action) {
+    case "add":
+      await addContact(name, email, phone);
+      break;
+    case "getContactById":
+      const contact = await getContactById(id);
+      console.log(contact);
+      break;
+    case "remove":
+      await removeContact(id);
+      break;
+    case "listContacts":
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+
+      default:
+          break;
+  }
+}
+
+// invokeAction({ action: "add", name: "Lewis", email: "lewis@mail.com", phone: "333 333 333" });
+// invokeAction({ action: "listContacts" });
+// invokeAction({ action: "getContactById", id: "1" });
+// invokeAction({ action: "remove", id: "c0ph" });
